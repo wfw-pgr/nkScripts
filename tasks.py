44fc008101d176ed4a8209093abecbb2a652d493
wfw-pgr <wfw.pgr@gmail.com>
2025-04-15 00:10:51 +0900

    update

--- tasks.py
+++ tasks.py
@@ -65,7 +65,7 @@ def post( ctx ):
     command1 = "for f in `ls out/*.eps`; do gs -dSAFER -dEPSCrop "\
         "-sDEVICE=pdfwrite -o ${f%.eps}_%d.pdf ${f};done"
     command2 = "mogrify -background white -alpha off -density 400 "\
-        "-resize 50%x50% -path png -format png out*.pdf"
+        "-resize 50%x50% -path png -format png out/*.pdf"
     subprocess.run( command1, shell=True )
     subprocess.run( command2, shell=True )
     return()
